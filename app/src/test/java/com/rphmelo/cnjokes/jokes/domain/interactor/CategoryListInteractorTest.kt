c7912c4dfb070ab85b43ea25ff0b534201b40acb
Rphmelo <raphael_soft@hotmail.com>
2019-11-24 21:09:10 -0300

    delete interactor tests

--- app/src/test/java/com/rphmelo/cnjokes/jokes/domain/interactor/CategoryListInteractorTest.kt
+++
@@ -1,9 +0,0 @@
-package com.rphmelo.cnjokes.jokes.domain.interactor
-
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
-
-@RunWith(JUnit4::class)
-class CategoryListInteractorTest {
-
-}
